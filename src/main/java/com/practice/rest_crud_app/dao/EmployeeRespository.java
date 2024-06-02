a0690e8625cffbdcfa17d8fa6ecad42c0347dfce
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-06-02 15:55:20 +0530

    Using spring-data-rest dependency

--- src/main/java/com/practice/rest_crud_app/dao/EmployeeRespository.java
+++ src/main/java/com/practice/rest_crud_app/dao/EmployeeRespository.java
@@ -1,9 +1,12 @@
 package com.practice.rest_crud_app.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.practice.rest_crud_app.entity.Employee;
 
+//@RepositoryRestResource(path="customEndPoint")
+// url will be localhost:8080/api/v1/customEndPoint
 public interface EmployeeRespository extends JpaRepository<Employee, Integer> {
 
 }
